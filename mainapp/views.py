fce63b1f6307fb36eb8fdd63b2ae5f5473a82654
Aarav Jain <jainaarav552@gmail.com>
2023-12-17 15:01:44 +0530

    modified:   db.sqlite3 	modified:   mainapp/admin.py 	modified:   mainapp/models.py 	modified:   mainapp/views.py 	modified:   templates/index.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .models import Reviews
 
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        name = request.POST.get(str('name'))
+        class_and_section = request.POST.get(str('class_and_section'))
+        recommendations = request.POST.get(str('recommendations'))
+
+        reviews = Reviews(name=name, class_and_section=class_and_section, recommendations=recommendations)
+        reviews.save()
+
     return render(request, 'index.html')
