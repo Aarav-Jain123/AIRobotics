0fa0fdaacb489052af396a9ac68441b53f748df7
Aarav Jain <jainaarav552@gmail.com>
2023-12-17 16:40:18 +0530

    modified:   .gitignore 	modified:   mainapp/views.py 	modified:   templates/index.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from .models import Reviews
+from django.contrib import messages
 
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
         name = request.POST.get(str('name'))
-        class_and_section = request.POST.get(str('class_and_section'))
+        class_and_section = request.POST.get(str('class-and-section'))
         recommendations = request.POST.get(str('recommendations'))
 
         reviews = Reviews(name=name, class_and_section=class_and_section, recommendations=recommendations)
         reviews.save()
+
+        messages.success(request, message='Your message was stored successfully!')
 
     return render(request, 'index.html')
